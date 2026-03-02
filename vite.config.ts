ae6d4b758552d125324bb932a90599a269cd3b7b
Shivangi <shivangichaurasia1515@gmail.com>
2026-03-02 23:35:13 +0530

    Updated

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="node" />
+// @ts-ignore
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+   root: 'frontend', 
+  plugins: [react()],
+   build: {
+    outDir: 'frontend/dist', 
+  },
+})
