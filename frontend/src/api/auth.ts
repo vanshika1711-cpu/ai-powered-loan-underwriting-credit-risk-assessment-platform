f394f9476e72c5f4e4302254512144f0bfd33a40
Shivangi <shivangichaurasia1515@gmail.com>
2026-03-04 20:34:04 +0530

    Temp Fix

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL;
+const API = "https://ai-powered-loan-underwriting-credit-risk-3at2.onrender.com/";
 
 export const loginUser = async (email: string, password: string) => {
   const response = await axios.post(`${API}/login`, {
