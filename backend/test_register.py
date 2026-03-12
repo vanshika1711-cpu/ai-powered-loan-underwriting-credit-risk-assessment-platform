5a68202b0848f17a50bcd755ca736a9e813b9089
Shivangi <shivangichaurasia1515@gmail.com>
2026-03-13 00:28:13 +0530

    Added Render url

--- backend/test_register.py
+++ backend/test_register.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://127.0.0.1:5000/register"
+url = "https://ai-powered-loan-underwriting-credit-risk-3at2.onrender.com//register"
 
 data = {
     "email": "admin@gmail.com",
