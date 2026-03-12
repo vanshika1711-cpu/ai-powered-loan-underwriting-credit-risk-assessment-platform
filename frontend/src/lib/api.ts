514b9625ab569b2581b7ce014711c3b1d1f3cc6e
Shivangi <shivangichaurasia1515@gmail.com>
2026-03-13 00:33:52 +0530

    Fixed api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL || "http://127.0.0.1:5000";
+const API = import.meta.env.VITE_API_URL || "https://ai-powered-loan-underwriting-credit-risk-3at2.onrender.com/";
 
 const api = axios.create({
   baseURL: API,
